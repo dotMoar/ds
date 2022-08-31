fe289a31851bef1474d723c2a395736a1b7b5546
sebastian.diaz@toctoc.com <sebastian.diaz@toctoc.com>
2022-08-31 01:24:15 -0400

    corregir yml con publicacion y version

--- src/components/atoms/Buttons/Button.tsx
+++ src/components/atoms/Buttons/Button.tsx
@@ -24,7 +24,7 @@ const Button: FC<iProps> = ({
 						sx={customStyleObject}
 						fullWidth={withBoolean}
 					>
-						{children}{texto}ea
+						{children}{texto}easac
 					</ButtonMUI>
 				</div>
 
