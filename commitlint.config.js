44bbe7b0bc032b62f5c832fb8d8ba54f19dd0240
sebastian.diaz@toctoc.com <sebastian.diaz@toctoc.com>
2022-09-02 16:45:20 -0400

    fix(homepage): fixed max body lenght 10k

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    rules: { 'footer-max-line-length': [1, 'always', 1000] },
+    rules: { 
+        'footer-max-line-length': [1, 'always', 1000],
+        'body-max-line-length': [1,'always',10000]
+ },
     parserPreset: { parserOpts: { noteKeywords: ['\\[.+\\]:'] } },
     ignores: [(message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message)]
   }
