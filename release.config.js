85d63cb71a99645a6a7a116b269fbd0702926c46
sebastian.diaz@toctoc.com <sebastian.diaz@toctoc.com>
2022-09-02 17:41:18 -0400

    fix(homepage): fix npm

--- release.config.js
+++ release.config.js
@@ -15,12 +15,15 @@ module.exports = {
           changelogFile: 'CHANGELOG.md'
         }
       ],
-      '@semantic-release/npm',
+      ["@semantic-release/npm", {
+        "npmPublish": false,
+        "tarballDir": "dist",
+      }],
       '@semantic-release/github',
       [
         '@semantic-release/git',
         {
-          assets: ['CHANGELOG.md', 'dist/**'],
+          assets: ['CHANGELOG.md', 'dist/**', 'package.json'],
           message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
         }
       ],
