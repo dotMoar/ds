726e381ac954cf47d4983d19575f04118e671164
sebastian.diaz@toctoc.com <sebastian.diaz@toctoc.com>
2022-09-02 11:55:17 -0400

    eaea

--- src/components/atoms/Buttons/Button.tsx
+++ src/components/atoms/Buttons/Button.tsx
@@ -24,7 +24,7 @@ const Button: FC<iProps> = ({
 						sx={customStyleObject}
 						fullWidth={withBoolean}
 					>
-						{children}{texto}easac1
+						{children}{texto} factoresS
 					</ButtonMUI>
 				</div>
 
