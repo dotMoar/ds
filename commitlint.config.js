325f6f49e0ec1b5b4c238280125d18aebdefc639
sebastian.diaz@toctoc.com <sebastian.diaz@toctoc.com>
2022-09-02 16:08:31 -0400

    fix(homepage): fixed deploy = try 1000 lines

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    rules: { 'footer-max-line-length': [1, 'always', 100] },
+    rules: { 'footer-max-line-length': [1, 'always', 1000] },
     parserPreset: { parserOpts: { noteKeywords: ['\\[.+\\]:'] } },
     ignores: [(message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message)]
   }
