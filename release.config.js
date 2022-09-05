7501f869046216925132bc13fe78016308b2c499
Seva <sebastian.diaz@toctoc.com>
2022-09-05 19:22:52 -0400

    fix(commitlint): se eliminan ramas que no existen 4

--- release.config.js
+++ release.config.js
@@ -1,31 +1,34 @@
 module.exports = {
-    branches: [
-      'main',
+  branches: [
+    { name: 'main' },
+    //{ name: 'pre/rc', channel: 'pre/rc', prerelease: 'rc' }, // `prerelease` is built with the template `${name.replace(/^pre\\//g, "")}`
+    { name: 'beta', channel: 'beta', prerelease: true }, // `prerelease` is set to `beta` as it is the value of `name`
+    //'main',
+    //{
+    //  name: 'beta',
+    //  prerelease: true
+    //}
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
       {
-        name: 'beta',
-        prerelease: true
+        changelogFile: 'CHANGELOG.md'
       }
     ],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      [
-        '@semantic-release/changelog',
-        {
-          changelogFile: 'CHANGELOG.md'
-        }
-      ],
-      ["@semantic-release/npm", {
-        "npmPublish": false
-      }],
-      '@semantic-release/github',
-      [
-        '@semantic-release/git',
-        {
-          assets: ['CHANGELOG.md', 'dist/**', 'package.json'],
-          message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-        }
-      ],
-      
-    ]
-  }
+    ["@semantic-release/npm", {
+      "npmPublish": false
+    }],
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'dist/**', 'package.json'],
+        message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+
+  ]
+}
