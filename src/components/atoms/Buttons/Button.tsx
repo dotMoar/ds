0330358950e023cf7ad5ac510cf2499a03353e47
Seva <sebastian.diaz@toctoc.com>
2022-09-06 13:37:45 -0400

    feat(git): esto es el merge (#5)

--- src/components/atoms/Buttons/Button.tsx
+++ src/components/atoms/Buttons/Button.tsx
@@ -24,6 +24,7 @@ const Button: FC<iProps> = ({
 						sx={customStyleObject}
 						fullWidth={withBoolean}
 					>
+						sad adsasd
 						{children}{texto} Hola soy un boton en otra rama
 						oye mas texto
 						cambio aca
